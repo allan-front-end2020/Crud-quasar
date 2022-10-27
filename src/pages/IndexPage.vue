053a502b2958286422c4f0df354dadf6a57289ac
Allan Tavares <allan.tavares@estudante.ufla.br>
2022-10-27 09:59:33 -0300

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,78 @@
+<template>
+  <q-page padding>
+    <q-table
+      title="Usuarios"
+      :rows="posts"
+      :columns="columns"
+      row-key="name"
+    >
+    <template v-slot:top>
+       <span class="text-h4">Usúarios</span>
+      <q-space />
+      <q-btn color="primary"  label="Novo usuario" :to="{ name: 'formPost' }" />
+    </template>
+     <template v-slot:body-cell-actions="props">
+      <q-td :props='props'>
+        <q-btn icon='delete'  color='negative' dense @click="handleDeletePost(props.row.id)"/>
+      </q-td>
+    </template>
+    </q-table>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref, onMounted } from 'vue'
+import postsService from 'src/services/posts'
+import { useQuasar } from 'quasar'
+
+export default defineComponent({
+  name: 'IndexPage',
+  setup () {
+    const posts = ref([])
+    const { list, remove } = postsService()
+    const columns = [
+
+      { name: 'id', field: 'id', label: 'Id', sortable: true, aling: 'left' },
+      { name: 'title', field: 'title', label: 'Titulo', sortable: true },
+      { name: 'author', field: 'author', label: 'Autor', sortable: true },
+      { name: 'actions', field: 'actions', label: 'Ações', aling: 'rigth' }
+
+    ]
+    const $q = useQuasar()
+    onMounted(() => {
+      getPosts()
+    })
+
+    const getPosts = async () => {
+      try {
+        const { data } = await list()
+        posts.value = data
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    const handleDeletePost = async (id) => {
+      try {
+        $q.dialog({
+          title: 'Remover',
+          message: 'Deseja remover esse usuario?',
+          cancel: true,
+          persistent: true
+        }).onOk(async () => {
+          await remove(id)
+          $q.notify({ message: 'Apagado com sucesso', icon: 'check', color: 'positive ' })
+          await getPosts()
+        })
+      } catch (error) {
+        $q.notify({ message: 'Erro ao apagar', icon: 'check', color: 'positive ' })
+      }
+    }
+
+    return {
+      posts,
+      columns,
+      handleDeletePost
+    }
+  }
+})
+</script>
